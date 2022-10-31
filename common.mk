5811788a3ebf1e55ed3f76db544e1fbf9ac1471d
LuK1337 <priv.luk@gmail.com>
2022-10-31 21:59:52 +0100

    sm6475-common: overlay: Configure aux camera for Aperture
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Change-Id: I437d5b8c5b29f037e5ac94a3d80d32cc0e42f1c2

--- common.mk
+++ common.mk
@@ -230,6 +230,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 PRODUCT_PACKAGES += \
     FrameworksResCommon \
+    LineageApertureAppCommon \
     LineageSdkCommon \
     SettingsProviderResCommon \
     SystemUIResCommon \
