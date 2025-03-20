e5af6a51c1f31ab6b3c2299c7bbe4dbcb4940fee
dianlujitao <dianlujitao@lineageos.org>
2025-03-20 22:04:37 +0800

    sm6475-common: Retain old audio AIDL version
    Simply replacing the AIDL interface with a different version leads to
    segmentation faults.
    
    Change-Id: Iad5fd10592ec76c7daf6f4f133a7e6b1e773f3de

--- common.mk
+++ common.mk
@@ -63,6 +63,7 @@ PRODUCT_PACKAGES += \
     libagm_compress_plugin \
     libagm_mixer_plugin \
     libagm_pcm_plugin \
+    libalsautilsv2.vendor \
     libbatterylistener \
     libbundleaidl \
     libdownmixaidl \
@@ -76,7 +77,8 @@ PRODUCT_PACKAGES += \
     libreverbaidl \
     libsndcardparser \
     libvisualizeraidl \
-    libvolumelistener
+    libvolumelistener \
+    qti-audio-types-aidl-V1-ndk.vendor
 
 AUDIO_HAL_DIR := hardware/qcom-caf/sm8450-6.6/audio/primary-hal
 
