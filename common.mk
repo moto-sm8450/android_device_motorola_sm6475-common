efa638da7bf5b106c1207aa85048652ddf4ffda0
LuK1337 <priv.luk@gmail.com>
2025-08-31 22:42:50 +0200

    sm6475-common: Unblob audio/audio effects vintf manifests
    Test: `m nothing` on 23.0
    Change-Id: Ia8c635fb58ff8930595dcd88946981e2ccce0907

--- common.mk
+++ common.mk
@@ -397,7 +397,8 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/motorola/vintf/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix_aidl.xml
-DEVICE_MANIFEST_FILE += device/motorola/sm6475-common/vintf/manifest.xml
+DEVICE_MANIFEST_FILE += device/motorola/sm6475-common/vintf/manifest.xml \
+    $(AUDIO_HAL_DIR)/configs/parrot/manifest_audio_qti_services.xml
 
 # Virtualization
 $(call inherit-product, packages/modules/Virtualization/apex/product_packages.mk)
