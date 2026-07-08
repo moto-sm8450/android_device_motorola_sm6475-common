c7af859a7bd6fbedf15f479ccf6fa4e89e7f9e1a
Nolen Johnson <johnsonnolen@gmail.com>
2026-07-08 13:32:40 -0400

    sm7435-common: Use Lineage charger
    Change-Id: I76caaca0616ec2b2e7af9b16b29eaf6ffb00b1a0

--- common.mk
+++ common.mk
@@ -144,7 +144,6 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     fstab.qcom.vendor_ramdisk \
     fstab.qcom.zram \
-    init.mmi.charge_only.rc \
     init.mmi.chipset.rc \
     init.mmi.rc \
     init.qcom.rc \
