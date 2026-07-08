76a223818c4e75d32f0318fb672781497e0c66d3
Nick Reuter <nreuter85@gmail.com>
2026-07-08 13:35:22 -0500

    sm6475-common: Switch to common gesture service
    Change-Id: Ic2c01242e31871d7ea109fea0e1dd72a1339083b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -218,9 +218,6 @@ ENABLE_VENDOR_RIL_SERVICE := true
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include hardware/motorola/sepolicy/qti/SEPolicy.mk
-BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
-PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
-PRODUCT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
